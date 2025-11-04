1e15c05186a9c680d76f8f32b3ee5d78700f3aab
HanSD632 <hansangdo6@gmail.com>
2025-11-04 15:38:06 +0900

    지형 생성, 풀, 물

---
+++ PerllinNoiseHSD/Assets/Scripsts/NoiseVoxelMap.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+
+public class NoiseVoxelMap : MonoBehaviour
+{
+    public GameObject blockPrefab;
+    public GameObject grassPrefab;
+    public GameObject waterPrefab;
+
+    public int width = 20;
+    public int depth = 20;
+    public int maxHeight = 16;
+    public int waterLevel = 5;
+
+    [SerializeField] float noiseScale = 20f;
+
+    void Start()
+    {
+        float offsetX = Random.Range(-9999f, 9999f);
+        float offsetZ = Random.Range(-9999f, 9999f);
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int z = 0; z < depth; z++)
+            {
+                float nx = (x + offsetX) / noiseScale;
+                float nz = (z + offsetZ) / noiseScale;
+
+                float noise = Mathf.PerlinNoise(nx, nz);
+                Debug.Log($"{x}, " + $"{z}  " + noise);
+
+                int h = Mathf.FloorToInt(noise * maxHeight);
+
+                if (h <= 0) continue;
+
+                for (int y = 0; y <= h; y++)
+                {
+                    if (y < h)
+                        Place(x, y, z);
+                    else 
+                        PlaceGrass(x, y, z);
+                }
+
+                for (int y = h+1; y <= waterLevel; y++)
+                {
+                    PlaceWater(x, y, z);
+                }
+            }
+        }
+    }
+
+    private void Place(int x, int y, int z)
+    {
+        var go = Instantiate(blockPrefab, new Vector3(x, y, z), Quaternion.identity, transform);
+        go.name = $"b_{x}_{z}_{y}";
+    }
+
+    private void PlaceGrass(int x, int y, int z)
+    {
+        var go = Instantiate(grassPrefab, new Vector3(x, y, z), Quaternion.identity, transform);
+        go.name = $"b_{x}_{z}_{y}";
+    }
+    private void PlaceWater(int x, int y, int z)
+    {
+        var go = Instantiate(waterPrefab, new Vector3(x, y, z), Quaternion.identity, transform);
+        go.name = $"b_{x}_{z}_{y}";
+    }
+}
